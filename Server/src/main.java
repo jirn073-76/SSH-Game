6a627f01989f21d1c08d388ef706d417e123bf23
Hugo Bayer <hugo.bayer@studierende.htl-donaustadt.at>
2019-10-24 12:47:37 +0200

    Commented some unnessesary code in main

--- Server/src/main.java
+++ Server/src/main.java
@@ -2,29 +2,29 @@
 public class main {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello, world (From Armin)!");
-		System.out.println("Treffen sich zwei, der eine kommt nicht. ");
-		
-		
-		
-		Playarea p = new Playarea(10,10);
-		p.newPlayer();
-		StringBuilder sb = new StringBuilder();
-		char[][] arr = p.getPlayareaAsCharArray();
-		System.out.println(sb.toString());
-		for (int i = 0; i < 10; i++) {
-			if(i==3)
-				p.players.get(0).getFunctions().changeDirection(Direction.right);;
-			p.update();
-			sb = new StringBuilder();
-			arr = p.getPlayareaAsCharArray();
-			for(int y = 0; y < arr.length;y++)
-			{
-				for(int x = 0; x < arr[0].length; x++)
-					sb.append(arr[x][y]);
-				sb.append('\n');
-			}
-			System.out.println(sb.toString());
+//		System.out.println("Hello, world (From Armin)!");
+//		System.out.println("Treffen sich zwei, der eine kommt nicht. ");
+//		
+//		
+//		
+//		Playarea p = new Playarea(10,10);
+//		p.newPlayer();
+//		StringBuilder sb = new StringBuilder();
+//		char[][] arr = p.getPlayareaAsCharArray();
+//		System.out.println(sb.toString());
+//		for (int i = 0; i < 10; i++) {
+//			if(i==3)
+//				p.players.get(0).getFunctions().changeDirection(Direction.right);;
+//			p.update();
+//			sb = new StringBuilder();
+//			arr = p.getPlayareaAsCharArray();
+//			for(int y = 0; y < arr.length;y++)
+//			{
+//				for(int x = 0; x < arr[0].length; x++)
+//					sb.append(arr[x][y]);
+//				sb.append('\n');
+//			}
+//			System.out.println(sb.toString());
 		}		
 	}
 }
