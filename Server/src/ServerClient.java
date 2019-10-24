5d8afb5ce4ac912bf6bfb99a5c4c37c6d43b016c
Dionis Ramadani <dionis.ramadani@studierende.htl-donaustadt.at>
2019-10-24 13:17:34 +0200

    eZ geh ma

--- Server/src/ServerClient.java
+++ Server/src/ServerClient.java
@@ -1,6 +1,26 @@
 import org.apache.sshd.server.*;
+import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
+import org.apache.sshd.server.scp.ScpCommandFactory;
+import org.apache.sshd.server.shell.ProcessShellFactory;
 
 public class ServerClient {
-	SshServer sshd = SshServer.setUpDefaultServer();
+	
+	private SshServer sshd;
 
+	public SshServer getSshd() {
+		return sshd;
+	}
+	
+	public void setSshd(SshServer sshd) {
+		this.sshd = sshd;
+	}
+	
+	public ServerClient(int port) {
+		setSshd(SshServer.setUpDefaultServer());
+        sshd.setHost("127.0.0.1");
+		sshd.setPort(port);
+		sshd.setKeyPairProvider(new SimpleGeneratorHostKeyProvider());
+		//sshd.setShellFactory(new ProcessShellFactory(new String[] { "/bin/sh", "-i", "-l" }));
+		sshd.setCommandFactory(new ScpCommandFactory());
+	}
 }
